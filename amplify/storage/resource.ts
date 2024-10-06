6f9ee0fd7cd32e8f093e4417ef6cceffd202eb9f
James <jamesmaino@gmail.com>
2024-10-07 10:27:51 +1100

    use private only

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,15 +3,15 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "thylation-images-test",
   access: (allow: any) => ({
-    'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
-    'media-readonly/*': [allow.authenticated.to(['read'])],
-    'shared-folder-readwrite/*': [
-      allow.authenticated.to(['read', 'write'])
-    ],
-    'protected-useronlyreadwritedelete/{entity_id}/*': [
-      allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
+    // 'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+    // 'media-readonly/*': [allow.authenticated.to(['read'])],
+    // 'shared-folder-readwrite/*': [
+    //   allow.authenticated.to(['read', 'write'])
+    // ],
+    // 'protected-useronlyreadwritedelete/{entity_id}/*': [
+    //   allow.authenticated.to(['read']),
+    //   allow.entity('identity').to(['read', 'write', 'delete'])
+    // ],
     'private-useronlyreadwritedelete/{entity_id}/*': [
       allow.entity('identity').to(['read', 'write', 'delete'])
     ]
